8c80b947e26557fde9129a14cecaed31dd897ee4
Kareem1715 <Kareem.H1715@gmail.com>
2023-10-08 15:18:59 +0300

    Task 6 - print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for col in row:
-            print('{}'.format(col), end='')
+            print('{:d}'.format(col), end='')
             if col + 1 in row:
                 print(end=' ')
         print()
