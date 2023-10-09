e62a0fe787bd9ba9d59993ca6d2c204568f3a1f9
Kareem1715 <Kareem.H1715@gmail.com>
2023-10-09 20:58:19 +0300

    Task 13 - palindrome list

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,55 +1,71 @@
 #include "lists.h"
 
 /**
- * is_palindrome - check palindrome
+ * reverse_listint - reverses a linked list
+ * @head: pointer to the first node in the list
+ * Return: pointer to the first node in the new list
+ */
+void reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
+
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+
+	*head = prev;
+}
+
+/**
+ * is_palindrome - checks if a linked list is a palindrome
+ * @head: double pointer to the linked list
  *
- * @head: The head of linked list
- *
- * Description: This function check if the given linked list
- * is palindrome or not.
- *
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome.
+ * Return: 1 if it is, 0 if not
  */
 int is_palindrome(listint_t **head)
 {
-	int n = 0, m = 0, start, j = 0, arr[n / 2];
-	listint_t *trv = *head;
+	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
 
-	if (!*head)
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
-	while (trv)
-	{
-		trv = trv->next;
-		n++;
-	}
-	if (n % 2)
-		start = (n / 2) + 2;
-	else
-		start = (n / 2) + 1;
-	trv = *head;
-	arr[n / 2] = arr[n / 2];
-	j = (n / 2) - 1;
 
-	while (trv)
+	while (1)
 	{
-		if (m == start - 1)
-			break;
-		arr[j] = trv->n;
-		m++;
-		j--;
-		trv = trv->next;
-	}
-
-	j = 0;
-	while (trv)
-	{
-		if (trv->n == arr[j])
+		fast = fast->next->next;
+		if (!fast)
 		{
-			trv = trv->next;
-			j++;
+			dup = slow->next;
+			break;
+		}
+		if (!fast->next)
+		{
+			dup = slow->next->next;
+			break;
+		}
+		slow = slow->next;
+	}
+
+	reverse_listint(&dup);
+
+	while (dup && temp)
+	{
+		if (temp->n == dup->n)
+		{
+			dup = dup->next;
+			temp = temp->next;
 		}
 		else
 			return (0);
 	}
-	return (1);
+
+	if (!dup)
+		return (1);
+
+	return (0);
 }
