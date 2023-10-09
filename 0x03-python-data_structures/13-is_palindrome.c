5831cafe7a3cde8b96c29c94cb69378766647a7a
Kareem1715 <Kareem.H1715@gmail.com>
2023-10-09 23:50:42 +0300

    Task 13 - palindrome list

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -13,37 +13,29 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *trv = *head;
-	int n = 0, m = 0, start, j = 0, k = 0, arr[1000];
+	int n = 0, j = 0, k = 0, arr[200];
 
-	if (!*head || !(*head)->next)
+	if (!*head || !(*head)->next) /* If List is empty or list has one value */
 		return (1);
-	while (trv)
-	{
-		trv = trv->next;
-		n++;
-	}
-	start = (n / 2); /* 2 */
-	j = start - 1; /* 3 */
-	trv = *head;
-	while (trv)
-	{
-		if (m == start) /* m = 0, start = 4 */
-			break;
-		arr[j] = trv->n;
-		m++, j--; 
-		trv = trv->next;
 
+	while (trv) /* Count the length of list (n) */
+		trv = trv->next, n++;
+	trv = *head; /* Make (trv) point to first node */
+
+	j = (n / 2) - 1; /* j is last index of array */
+	while (trv && j + 1)
+	{
+		arr[j--] = trv->n; /* Fill the array from last to first */
+		trv = trv->next;
 	}
-	(n % 2 ? (trv = trv->next) : trv);
+
+	(n % 2 ? (trv = trv->next) : trv); /* If n is odd move to next node */
 	while (trv)
 	{
-		if (trv->n == arr[k])
-		{
-			trv = trv->next;
-			k++;
-		}
-		else
+		if (trv->n == arr[k]) /* k start from 0 */
+			trv = trv->next, k++;
+		else /* If The (node value) and (array value) not equal at any index */
 			return (0);
 	}
-	return (1);
+	return (1); /* If previous while loop end without goes to else condition */
 }
