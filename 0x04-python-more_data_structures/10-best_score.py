f87c55577523e950c3574a23f1e43d00dca98c65
Kareem1715 <Kareem.H1715@gmail.com>
2023-10-12 01:53:30 +0300

    Task 10 - best scoer

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if not a_dictionary:
         return None
-    return max(a_dictionary)
+    for key, value in a_dictionary.items():
+        if value == max(a_dictionary.values()):
+            return key
