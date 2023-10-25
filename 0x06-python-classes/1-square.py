78521ee10abe92015769ae7dbfeb8bc1c2231307
Kareem1715 <Kareem.H1715@gmail.com>
2023-10-26 01:10:36 +0300

    Task 1 - intialize square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This class defines a square by: (based on 0-square.py)"""
+
+
+class Square:
+    """Exmaple of docstring.
+    This class for represent the square"""
+    def __init__(self, size):
+        """Intialize square
+        Args:
+            size (int): size of square
+        """
+        self.__size = size
