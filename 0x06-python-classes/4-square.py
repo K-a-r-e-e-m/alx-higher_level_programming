c14f3a58ce63bbbf5db7f6957b05d2015eb90b27
Kareem1715 <Kareem.H1715@gmail.com>
2023-10-26 02:28:35 +0300

    Task 4 - Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""This class defines a square by: (based on 0-square.py)"""
+
+
+class Square:
+    """Exmaple of docstring.
+
+    This class for represent the square
+    """
+    def __init__(self, size=0):
+        """Intialize square
+        Args:
+            size (int): size of square private instance attribute.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieve the size (getter)"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set value to size (setter)"""
+
+        # The isinstance() is a built-in function used for type checking
+        # It verifies if an object is of a specified type
+        # returning True if it is, and False otherwise.
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """Return The current square area"""
+        return self.__size ** 2
