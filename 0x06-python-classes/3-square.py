284b3362f65e9394f0eef1d94e1fbc9bd1e0df55
Kareem1715 <Kareem.H1715@gmail.com>
2023-10-26 01:57:36 +0300

    Task 3 - Square area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This class defines a square by: (based on 0-square.py)"""
+
+
+class Square:
+    """Exmaple of docstring.
+
+    This class for represent the square
+    """
+    def __init__(self, size=0):
+        """Intialize square
+        Args:
+            size (int): size of square private instance attribute.
+        """
+        # The isinstance() is a built-in function used for type checking
+        # It verifies if an object is of a specified type
+        # returning True if it is, and False otherwise.
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
