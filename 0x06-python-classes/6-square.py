a709076a1555a2a507dc4e63e53b352ad07ade4a
Kareem1715 <Kareem.H1715@gmail.com>
2023-10-26 05:02:06 +0300

    Task 6 - print position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -21,7 +21,7 @@ class Square:
         return self.__size
 
     @size.setter
-    def size self, value:
+    def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -35,11 +35,10 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if (
-            not isinstance(value, tuple)
-            or len(value) != 2
-            or [v for v in value if isinstance(i, int) and i > 0]
-        ):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
