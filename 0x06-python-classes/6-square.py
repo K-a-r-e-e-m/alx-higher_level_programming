9322bba76a8eaf82aec7b0346c0774dae43ceabe
Kareem1715 <Kareem.H1715@gmail.com>
2023-10-26 04:20:53 +0300

    Task 6 - print position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -30,7 +30,7 @@ class Square:
             raise ValueError('size must be >= 0')
         self.__size = value
 
-  @property
+   @property
     def position(self):
         """Get/set the current position of the square."""
         return (self.__position)
