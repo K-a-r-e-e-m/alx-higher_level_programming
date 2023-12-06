604abac1ec49e93e12ab04db81e79b392af819a5
Kareem1715 <Kareem.H1715@gmail.com>
2023-12-06 19:28:29 +0200

    Task 4 - JSON

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module for json string
+"""
+import json
+
+
+def from_json_string(my_str):
+    '''This function returns an object (Python data structure)
+    represented by a JSON string'''
+    return json.loads(my_str)
