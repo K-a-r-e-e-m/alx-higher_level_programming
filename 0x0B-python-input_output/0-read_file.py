8f3f8f0cca14e2fb8278c421324657fc510c4a96
Kareem1715 <Kareem.H1715@gmail.com>
2023-12-06 14:35:44 +0200

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 '''This module for input and output files in python '''
 
+
 def read_file(filename=""):
     '''This method for read file'''
     with open(filename) as f:
-        print(f.read())
+        print(f.read(), end='')
