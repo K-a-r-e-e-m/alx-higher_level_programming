af5c75ed11dabfaaf63496fe7daaeba96f059694
Kareem1715 <Kareem.H1715@gmail.com>
2024-01-13 21:11:38 +0200

    Task 2 edit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,10 +7,10 @@ class Rectangle(Base):
     """This class for Rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """This is the intialization method of class"""
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
         super().__init__(id)
 
     @property
