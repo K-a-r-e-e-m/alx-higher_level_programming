616e44d6ac5e1fe22c19a860b49fe480cdd14694
Kareem1715 <Kareem.H1715@gmail.com>
2024-01-13 18:29:02 +0200

    Task 2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module for Rectangle class that inherits from Base"""
-from base import Base 
+from models.base import Base 
 
 
 class Rectangle(Base):
@@ -11,3 +11,4 @@ class Rectangle(Base):
         self.__height = height
         self.__x = x
         self.__y = y
+        super().__init__(id)
