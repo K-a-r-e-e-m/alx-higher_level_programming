9faf483beef90d6c58e537b80ae88fcda9dbe9d4
Kareem1715 <Kareem.H1715@gmail.com>
2024-02-13 00:13:46 +0200

    Task 10 - Factorial in JS

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
-function fact (num) { return ((num === 0  || !Number(num)) ? 1 : num * fact(num - 1)); }
+function fact (num) { return ((num === 0 || !Number(num)) ? 1 : num * fact(num - 1)); }
 console.log(fact(Number(process.argv[2])));
 /* You can use `isNaN()` instead of `!Number()` */
-
 
 // Another solution with loop
 // let fact = 1;
