d48542d64f70a0acde8f18687b6a68067f86a007
Kareem1715 <Kareem.H1715@gmail.com>
2024-02-12 21:56:48 +0200

    Task 3 - print argv

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
+console.log(process.argv[2] ? process.argv[2] : 'No argument');
 
-if (process.argv[2]) {
-  console.log(process.argv[2]);
-} else {
-  console.log('No argument');
-}
+// Another solution
+// if (process.argv[2]) {
+//   console.log(process.argv[2]);
+// } else {
+//   console.log('No argument');
+// }
