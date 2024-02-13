43b9a18a3a6e9b433578de0cf63bb99dee2a6e99
Kareem1715 <Kareem.H1715@gmail.com>
2024-02-14 01:19:24 +0200

    Task 5- super in inheritance

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    // calls the parent class's constructor with size
+    // provided for the Rectangle's width and height
+    super(size, size);
+  }
+}
+module.exports = Square;
