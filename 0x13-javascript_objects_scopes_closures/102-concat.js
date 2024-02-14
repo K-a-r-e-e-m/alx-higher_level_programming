48204121fbec22e114a1b94064b05f9041708c3b
Kareem1715 <Kareem.H1715@gmail.com>
2024-02-14 05:09:32 +0200

    Advanced 2 - Concat two files

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+/* fs --> File System module */
+const fileSys = require('fs');
+
+// Read a file and run it ./fileA ./fileB ./Filec
+const fileA = fileSys.readFileSync('./' + process.argv[2]);
+const fileB = fileSys.readFileSync('./' + process.argv[3]);
+fileSys.writeFileSync('./' + process.argv[4], fileA + fileB, 'utf-8');
+//               ''''''''''File'''''''', '''''Data'''', 'options'
