905785e91eabb1710d78efe66da36c7f23cc2732
Kareem1715 <Kareem.H1715@gmail.com>
2024-02-14 04:25:08 +0200

    Advanced 0 - map in JS

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-let myList = require('./100-data');
+const myList = require('./100-data');
 console.log(myList.list); // Before map
 
 const newList = myList.list.map((value, index) => value * index);
