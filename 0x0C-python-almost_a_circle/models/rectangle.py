39b6810e112df7c882a213437bed61279bb58c96
Kareem1715 <Kareem.H1715@gmail.com>
2024-02-16 21:42:48 +0200

    Add validation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,10 +7,10 @@ class Rectangle(Base):
     """This class for Rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """This is the intialization method of class"""
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property
@@ -21,6 +21,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, new_value):
         '''This is width setter decoratror'''
+        if type(new_value) is not int:
+            raise TypeError('width must be an integer')
+        if new_value <= 0:
+            raise ValueError('width must be > 0')
         self.__width = new_value
 
     @property
@@ -30,7 +34,11 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, new_value):
-        '''This is heigit setter decoratror'''
+        '''This is height setter decoratror'''
+        if type(new_value) is not int:
+            raise TypeError('height must be an integer')
+        if new_value <= 0:
+            raise ValueError('height must be > 0')
         self.__height = new_value
 
     @property
@@ -41,6 +49,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, new_value):
         '''This is x setter decoratror'''
+        if type(new_value) is not int:
+            raise TypeError('x must be an integer')
+        if new_value < 0:
+            raise ValueError('x must be >= 0')
         self.__x = new_value
 
     @property
@@ -51,4 +63,8 @@ class Rectangle(Base):
     @y.setter
     def y(self, new_value):
         '''This is y setter decoratror'''
+        if type(new_value) is not int:
+            raise TypeError('y must be an integer')
+        if new_value < 0:
+            raise ValueError('y must be >= 0')
         self.__y = new_value
