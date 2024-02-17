00c9f7b89ac0d1ca1af0ca6c0669961033bcea38
Kareem1715 <Kareem.H1715@gmail.com>
2024-02-17 23:11:00 +0200

    Task 3 - Safe from SQL injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
 
     cur = data_base.cursor()
     cur.execute('SELECT * FROM states\
-                WHERE name LIKE BINARY %s ORDER BY id', (sys.argv[4], ))
+                WHERE name LIKE BINARY %s ORDER BY id', (sys.argv[4]))
 
     rows = cur.fetchall()
     for i in rows:
