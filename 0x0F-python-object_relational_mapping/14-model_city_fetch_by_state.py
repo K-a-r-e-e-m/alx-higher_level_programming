0c364d721d38b85745d0225f5b33a57ad9e3b1b2
Kareem1715 <Kareem.H1715@gmail.com>
2024-02-18 23:59:05 +0200

    Task 14 - Cities in state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    query_result = session.query(City, State).join(State).order_by(City.id)
+    query_result = session.query(City, State).join(City).order_by(City.id)
 
     for city_row, state_row in query_result:
         print('{}: ({}) {}'.format(state_row.name, city_row.id, city_row.name))
