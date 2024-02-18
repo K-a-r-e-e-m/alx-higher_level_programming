900adeadb654384677eb6c95013493401c761d0f
Kareem1715 <Kareem.H1715@gmail.com>
2024-02-18 21:28:15 +0200

    Task 11 - Add state to table

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This script adds the State object “Louisiana” to
+the database hbtn_0e_6_usa
+"""
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+if __name__ == '__main__':
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query_result = session.query(State).order_by(State.id)
+
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+    session.commit()
+
+    for instance in query_result:
+        if instance.name == 'Louisiana':
+            print(instance.id)
