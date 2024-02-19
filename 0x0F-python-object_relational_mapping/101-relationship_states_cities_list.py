3c29389d1997fade82ef28d608525cfa91a5b052
Kareem1715 <Kareem.H1715@gmail.com>
2024-02-19 02:16:01 +0200

    Advanced 1 - List the state with its cities

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This script lists all State objects, and corresponding City objects,
+ contained in the database hbtn_0e_101_usa
+"""
+from relationship_state import State, Base
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+if __name__ == '__main__':
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query_result = session.query(State).order_by(State.id)
+
+    for inst in query_result:
+        print('{}: {}'.format(inst.id, inst.name))
+        for city in inst.cities:
+            print('    {}: {}'.format(city.id, city.name))
