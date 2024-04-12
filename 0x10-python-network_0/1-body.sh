8a82ead78a00fe9ebd76eeb177773ed36e1b94e0
Kareem1715 <Kareem.H1715@gmail.com>
2024-04-12 22:16:28 +0200

    Task 1 - Body of 200 status code

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request to the URL, and displays the body of the response
-curl -X GET -s -L $1
+curl -X GET -s -L $1 # -L that follow redirection until reach to final distenation in url
