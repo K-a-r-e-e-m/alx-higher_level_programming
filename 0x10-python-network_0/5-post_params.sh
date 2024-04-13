5ecd7991d83e94ac2e712c71c48b02ac0fe7e3d6
Kareem1715 <Kareem.H1715@gmail.com>
2024-04-13 06:29:39 +0200

    Task 5 - Sends a POST request

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a POST request to the URL and display body response
-curl -sd "email=gmail.com&subject=I will always be here for PLD" $1
+curl -sd "email=test@gmail.com&subject=I will always be here for PLD" $1
