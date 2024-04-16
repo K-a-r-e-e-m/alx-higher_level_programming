0642ac085a4919d96b16527efd4a5e388bc874cf
Kareem1715 <Kareem.H1715@gmail.com>
2024-04-16 21:13:09 +0200

    6-peak.txt

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -12,21 +12,4 @@ def find_peak(list_of_integers):
     if list_of_integers == []:
         return
 
-    check1 = True
-    check2 = True
-    peak = list_of_integers[0]
-
-    for i in range(1, len(list_of_integers) - 1):
-
-        if list_of_integers[i - 1] <= list_of_integers[i]:
-            peak = list_of_integers[i]
-
-        if list_of_integers[i] >= list_of_integers[i + 1]:
-            peak = list_of_integers[i]
-
-    if list_of_integers[0] > peak:
-        return list_of_integers[0]
-    elif list_of_integers[len(list_of_integers) - 1] > peak:
-        return list_of_integers[len(list_of_integers) - 1]
-    else:
-        return peak
+    return max(list_of_integers)
