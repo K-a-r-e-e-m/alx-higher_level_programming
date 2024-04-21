2689316acb932f7a18074e5ade0ff5d6f9e9ab44
Kareem1715 <Kareem.H1715@gmail.com>
2024-04-22 00:08:28 +0200

    Task 1 - Get value of header variable

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Sends a request to URL and displays the value of X-Request-Id variable'''
+import urllib.request
+import sys
+
+
+with urllib.request.urlopen(sys.argv[1]) as response:
+    body = response.read()
+
+print(response.getheader('X-Request-Id'))
