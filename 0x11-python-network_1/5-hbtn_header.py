af81b417ab4e03e59c96bfcef12df17df91c0211
Kareem1715 <Kareem.H1715@gmail.com>
2024-04-22 22:21:26 +0200

    Task 5 - Fetch url and get header value

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 '''displays the value of the variable X-Request-Id in the response header'''
-from requests import get
+from requests import get, HTTPError
 from sys import argv
 
 if __name__ == '__main__':
     try:
         req = get(argv[1])
         print(req.headers['X-Request-Id'])
-    except Exception as er:
+    except  HTTPError as er:
         print(er)
