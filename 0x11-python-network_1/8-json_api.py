59029f5dd2b0d8541c36060ce9c093361ced9f74
Kareem1715 <Kareem.H1715@gmail.com>
2024-04-23 17:46:36 +0200

    Task 8 - POST JSON

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,10 +8,12 @@ from sys import argv
 
 if __name__ == '__main__':
 
-    if len(argv) < 2:  # NO argument passed
-        argv[1] = ''
+    if len(argv) > 1:
+        letter = argv[1]
+    else:
+        letter = ''
 
-    req = post('http://0.0.0.0:5000/search_user', data={"q": argv[1]})
+    req = post('http://0.0.0.0:5000/search_user', data={"q": letter})
 
     try:
         json_req = req.json()
