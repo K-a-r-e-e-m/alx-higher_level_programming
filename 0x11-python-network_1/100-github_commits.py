0b498f089061a3b33cf69f53205c0cb8c141a2cc
Kareem1715 <Kareem.H1715@gmail.com>
2024-04-25 17:36:38 +0200

    Advanced - List the least 10 commits for repo

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,12 +10,8 @@ if __name__ == '__main__':
     req = get(f'https://api.github.com/repos/{argv[2]}/{argv[1]}/commits')
     json_data = req.json()
 
-    try:
-        for i in range(10):
-            sha = json_data[i].get('sha')
-            author_name = json_data[i].get('commit').get('author').get('name')
+    for i in range(10):
+        sha = json_data[i].get('sha')
+        author_name = json_data[i].get('commit').get('author').get('name')
 
-            print(f'{sha}: {author_name}')
-
-    except IndexError:
-        pass
+        print(f'{sha}: {author_name}')
