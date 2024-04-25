81da9bad4e56030273c1c9e1b15b80aa2fd344a0
Kareem1715 <Kareem.H1715@gmail.com>
2024-04-25 17:35:23 +0200

    Advanced - List the least 10 commits for repo

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -3,13 +3,19 @@
    and uses the GitHub API to display your id
 '''
 from requests import get
-# from requests.auth import HTTPBasicAuth
 from sys import argv
 
 if __name__ == '__main__':
-    # We also can user auth=HTTPBasicAuth(...) but remove it is shorhand
+
     req = get(f'https://api.github.com/repos/{argv[2]}/{argv[1]}/commits')
     json_data = req.json()
-    for i in range(10):
-        print(f'{json_data[i].get("sha")}: \
-              {json_data[i].get("commit").get("author").get("name")}')
+
+    try:
+        for i in range(10):
+            sha = json_data[i].get('sha')
+            author_name = json_data[i].get('commit').get('author').get('name')
+
+            print(f'{sha}: {author_name}')
+
+    except IndexError:
+        pass
