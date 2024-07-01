5bd1287561377488e206394427a6a88c18bb5c45
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-07-01 14:44:55 +0300

    Advanced 1 - Adds a new attribute to an object if it’s possible

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+
+def add_attribute(instance, name, value):
+    """This function that adds a new attribute to an object"""
+
+    if hasattr(instance, '__dict__'):
+        setattr(instance, name, value)
+    else:
+        raise TypeError("can't add new attribute")
+
+    '''
+    Some objects, like built-in types (integers, strings, etc.),
+    do not have a __dict__  because their attributes
+    (e.g., integer values, string contents) are not stored in
+    a dictionary-like structure.
+
+    However, instances of custom classes in Python have a __dict__
+    attribute by default. This __dict__ acts as a dictionary where
+    Python stores the instance variables and their values.
+    When you add a new attribute to an instance using
+    setattr(instance, name, value), Python stores this attribute
+    in instance.__dict__.
+
+    This allows instances of custom classes to ```dynamically```
+    add new attributes at runtime,
+    which is not possible with built-in types.
+    '''
