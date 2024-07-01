17dbe8a0567b04ae8dac250796fe7f2687e2e165
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-07-01 19:53:57 +0300

    Task 5 - Add __str__ method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -77,3 +77,8 @@ class Rectangle(Base):
         """Represent rectangle by #"""
         for _ in range(self.height):
             print('#' * self.width)
+
+    def __str__(self):
+        """Add string representation for a class"""
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - " \
+        f"{self.width}/{self.height}"
