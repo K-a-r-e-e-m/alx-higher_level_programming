6f32ab1c56d07823816b59e90f523e137b6aa849
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-07-03 00:11:37 +0300

    Add some tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -19,6 +19,31 @@ class TestBase(unittest.TestCase):
         n = Base()
         self.assertEqual(1, n.id)
 
+    def test_to_json_string(self):
+        '''test json string representation'''
+        dic = {'x': 2, 'width': 10, 'id': 1, 'height': 7, 'y': 8}
+        inst = Base().to_json_string([dic])
+        res = '[{"x": 2, "width": 10, "id": 1, "height": 7, "y": 8}]'
+        self.assertEqual(inst, res)
+
+    def test_to_json_string_more_dicts(self):
+        '''test json string representation'''
+        dic = [{'x': 2, 'width': 10, 'id': 1, 'height': 7, 'y': 8},
+                {'x': 3, 'width': 22, 'id': 3, 'height': 17, 'y': 0}]
+        inst = Base().to_json_string(dic)
+        res = '[{"x": 2, "width": 10, "id": 1, "height": 7, "y": 8}, ' + \
+        '{"x": 3, "width": 22, "id": 3, "height": 17, "y": 0}]'
+        self.assertEqual(inst, res)
+
+    def test_json_string_none(self):
+        '''Check if json string is none'''
+        inst = Base().to_json_string(None)
+        self.assertEqual(inst, '[]')
+
+    def test_json_string_empty(self):
+        '''Check if json string is empty'''
+        inst = Base().to_json_string({})
+        self.assertEqual(inst, '[]')
 
 if __name__ == '__main__':
     unittest.main()
