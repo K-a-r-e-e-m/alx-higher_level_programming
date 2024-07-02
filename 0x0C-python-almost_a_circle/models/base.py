429eeeba616e17be9844bd2c02f10953d9f69680
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-07-02 11:59:18 +0300

    Task 14 - JSON representation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,6 +5,7 @@ This class will be the base of all other classes.
 The goal of it is to manage id attribute in all your future classes
 and to avoid duplicating the same code (by extension, same bugs).
 """
+import json
 
 
 class Base:
@@ -18,3 +19,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''returns the JSON string representation of list_dictionaries'''
+        if list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
