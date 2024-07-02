9017d8c04ddaaa7289ee4f40fa7115adb7297fde
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-07-02 10:10:25 +0300

    Task 11 - Add getter and setter for square size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,7 +9,24 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         '''Constructor method that has special functino super()'''
         super().__init__(size, size, x, y, id)
+        self.__size = size
 
     def __str__(self):
         '''String representation of instance'''
         return f'[Square] ({self.id}) {self.x}/{self.y} - {self.width}'
+
+    @property
+    def size(self):
+        '''Getter for size'''
+        return self.__size
+    # size must be private to avoid recursion with size method
+
+    @size.setter
+    def size(self, newSize):
+        '''Setter for size'''
+        if type(newSize) is not int:
+            raise TypeError('width must be an integer')
+        if newSize <= 0:
+            raise ValueError('width must be > 0')
+        self.width = newSize
+        self.height = newSize
