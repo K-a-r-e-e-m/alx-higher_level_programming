28f076e1ec3be584f663502437adff339b3d70d0
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-07-02 10:21:13 +0300

    Task 11 - Add getter and setter for square size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,7 +9,6 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         '''Constructor method that has special functino super()'''
         super().__init__(size, size, x, y, id)
-        self.size = size
 
     def __str__(self):
         '''String representation of instance'''
