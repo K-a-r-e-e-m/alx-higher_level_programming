3d53596ae5504d444e23d03b8f337335a6861d0a
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-07-03 12:20:05 +0300

    Fix - fix some bugs in update method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -91,15 +91,15 @@ class Rectangle(Base):
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
-        if len(args) == 1:
+        if len(args) >= 1:
             self.id = args[0]
-        if len(args) == 2:
+        if len(args) >= 2:
             self.width = args[1]
-        if len(args) == 3:
+        if len(args) >= 3:
             self.height = args[2]
-        if len(args) == 4:
+        if len(args) >= 4:
             self.x = args[3]
-        if len(args) == 5:
+        if len(args) >= 5:
             self.y = args[4]
 
     def to_dictionary(self):
